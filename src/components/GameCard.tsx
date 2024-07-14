82060945e5013dbae3f35115a75d74185c1326b6
Chris Mac <lettheboycode@gmail.com>
2024-07-14 19:15:02 +1000

    Refactor- move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Game } from "../hooks/useGames";
+import { Game } from "../Entities/Game";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
