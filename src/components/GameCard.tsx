820ff9b721cc32200e715e5a1a1715e4f84217f8
Chris Mac <lettheboycode@gmail.com>
2024-07-14 19:08:23 +1000

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
@@ -22,7 +23,8 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name}
+          <Link to={"/games/" + game.slug}> {game.name}</Link>
+
           <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
